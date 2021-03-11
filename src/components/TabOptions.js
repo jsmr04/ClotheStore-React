83c1ee48150053c1196d63bc2762a72385b9a223
Jose Smith Marmolejos Rocha <jsmr04@gmail.com>
2021-03-11 14:23:41 -0500

    TabOptions touch-ups

--- src/components/TabOptions.js
+++ src/components/TabOptions.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
     },
     tabHighlight:{
         marginTop:5,
-        height:5,
+        height:3,
         width:'100%',
     },
     tabTouchable:{
@@ -28,26 +28,11 @@ export default ({ options, titleColor, highlightColor }) =>{
     return (
         <View style = { styles.tabContainer }>
             { options && options.map((option, key) =>(
-                
-                // <Text key = { key }  > { option.name }</Text>
                 <TouchableOpacity key = { `touch-${key}` } style = {styles.tabTouchable} onPress = { () => option.onPress(key) }>
                     <Text key = { `text-${key}` } style = { [styles.tabText, { color: option.checked ? highlightColor : titleColor }] }>{ option.name }</Text>
                     <View key = { `view-${key}` } style = { [styles.tabHighlight, { backgroundColor: option.checked ? highlightColor : titleColor }] } />
                 </TouchableOpacity>
             ))}
-
-                {/* <TouchableOpacity style = {styles.tabTouchable} onPress = { () => checkOption(0) }>
-                    <Text style = { [styles.tabText, { color: tabOptions[0].checked ? theme.COLORS.PRIMARY : theme.COLORS.TITLE }] }>{ tabOptions[0].name }</Text>
-                    <View style = { [styles.tabHighlight, { backgroundColor: tabOptions[0].checked ? theme.COLORS.PRIMARY : theme.COLORS.TITLE }] } />
-                </TouchableOpacity> */}
-                {/* <TouchableOpacity style = {styles.tabTouchable} onPress = { () => checkOption(1) }>
-                    <Text style = { [styles.tabText, { color: tabOptions[1].checked ? theme.COLORS.PRIMARY : theme.COLORS.TITLE }] } >{ tabOptions[1].name }</Text>
-                    <View style = { [styles.tabHighlight, { backgroundColor: tabOptions[1].checked ? theme.COLORS.PRIMARY : theme.COLORS.TITLE }] } />
-                </TouchableOpacity>
-                <TouchableOpacity style = {styles.tabTouchable} onPress = { () => checkOption(2) }>
-                    <Text style = { [styles.tabText, { color: tabOptions[2].checked ? theme.COLORS.PRIMARY : theme.COLORS.TITLE }] }>{ tabOptions[2].name }</Text>
-                    <View style = { [styles.tabHighlight, { backgroundColor: tabOptions[2].checked ? theme.COLORS.PRIMARY : theme.COLORS.TITLE }] } />
-                </TouchableOpacity> */}
          </View>
         
     )
