58e5c33cf279e67f50a25afe7643e8a8c6d518b2
Jose Smith Marmolejos Rocha <jsmr04@gmail.com>
2021-03-10 19:26:31 -0500

    working on search screen

--- src/screens/store/Search.js
+++ src/screens/store/Search.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { SearchBar } from 'react-native-elements'
 
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        paddingTop:45,
+    },
+    searchContainer:{
+        backgroundColor: 'transparent', 
+        borderWidth: 0.2,
     }
+
 })
 
 export default () => {
+    const [searchText, setSearchText] = useState('')
     return (
         <View style = { styles.container }>
-            <Text>I'm Search</Text>
+            <SearchBar style = {{ borderWidth: 0.1 }}
+                containerStyle = { styles.searchContainer }
+                inputContainerStyle = {{ backgroundColor: 'transparent' }}
+                placeholder = { 'Find products...' }
+                value = { searchText }
+                onChangeText = { (text)=> setSearchText(text) }
+                showCancel = { true }
+                searchIcon = {{ size:24 }}
+                cancelIcon = {{ size:24 }}
+            />
         </View>
     )
 }
