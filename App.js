8229b7a7caf725ac8e7fa6d0e62db4fe2cf44827
Arthur Ataide <arthurreisataide@gmail.com>
2021-03-19 11:22:44 -0400

    Cart Design done.

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import AppNavigator from './src/navigation/AppNavigator';
 import Toast from 'react-native-toast-message';
+import { Provider, connect } from 'react-redux';
 import FirebaseConfig from './src/backend/FirebaseConfig';
 
 export default function App() {
   return (
     <>
-      <AppNavigator/>
+      <Provider store={store}>
+        <AppNavigator/>
+      </Provider>
       <Toast ref={(ref) => Toast.setRef(ref)} />
     </>
   );
